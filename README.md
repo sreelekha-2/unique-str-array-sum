eab14690fbf270f3196ff09fc1508c04688fcfe8
sreelekha-2 <89189294+sreelekha-2@users.noreply.github.com>
2022-10-10 12:50:10 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# unique-str-array-sum
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-vzhmpy)
---
+++ index.js
@@ -0,0 +1,44 @@
+let str = 'abcdea';
+let c = 0;
+
+//1.with every method
+function uniqueStr(str) {
+  const result = str
+    .split('')
+    .every((ele, index) => str.indexOf(ele) === index);
+  return result;
+}
+
+console.log(uniqueStr(str));
+
+//with for loop
+function unique(str) {
+  for (i = 0; i < str.length; i++) {
+    if (str.indexOf(str[i]) !== str.lastIndexOf(str[i])) {
+      return false;
+    }
+  }
+  return true;
+}
+
+console.log(unique(str));
+
+//2.array sum
+const arr = [1, 2, 4, 34, 22];
+const largest = arr.reduce((max, curr) => {
+  if (max < curr) {
+    return curr;
+  } else {
+    return max;
+  }
+}, arr[i]);
+
+console.log(largest);
+const sum = arr.reduce((acc, curr) => {
+  if (curr !== largest) {
+    return acc + curr;
+  }
+  return acc;
+}, 0);
+console.log(sum);
+console.log(sum === largest);
